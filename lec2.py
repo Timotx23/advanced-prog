edd09b3a7a0461643655e32a5e508e9b38d85077
Timotx23 <t.g.hildenbrand@tilburguniversity.edu>
2026-02-09 18:52:57 +0100

    Update lec2.py

--- lec2.py
+++ lec2.py
@@ -7,17 +7,22 @@
 
 #Decorators
 # is a function that extends the ability of another function
-def my_decorator(func):
-    def wrapper(*args):
+def my_decorator(func):#ALWAYS get access to the function itself
+    def wrapper(name):# gets access tp the input variables of the function
         print("Before the function runs...")
-        result= func(*args)#Function say_hello                    
+        enhance="Jesus " + name ##this is a very simple representation of how it can be enhanced by using the 
+        result= func(enhance)#Function say_hello                    
         print("After the function runs...")
         return result
     return wrapper
 
-@my_decorator# this calls the decorator -> if this sytax is used it wont directly start with say_hello but instead the my_decorator is called which takes the say_hello as input 
+@my_decorator# this calls the decorator -> if this sytax is used it wont directly start with say_hello but instead the my_decorator is called which takes the say_hello as input  BUT its only called when say_hello is being called
 #Its essentially a hijacking system that allows me to not change the original function say_hello but instead leave it be and possible enhance it
 def say_hello(name):
     print(f"Hello! {name}")
 
 say_hello("Timo")
+
+#currying/ Partial functions
+
+
